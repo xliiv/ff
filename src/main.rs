999e1b0dad245aef7998003a875a77fc7b9371b4
Tymoteusz Jankowski <tymoteusz.jankowski@gmail.com>
2017-06-08 14:49:35 +0200

    Make space-dir optional

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,12 @@ pub mod core;
 use cli::*;
 
 
-// TODO::
+// TODO:
 // clippy
 // fmt
+// add new readme
 // replace tracking-dir with sync-dir (inc. readme)
-// errors handling leavings
+
 // adding(removing) dir? should walk over each file and `add` (`remove`) them
 // bash autocompletion: https://kbknapp.github.io/clap-rs/clap/struct.App.html#examples-35
 // make Config trait?
